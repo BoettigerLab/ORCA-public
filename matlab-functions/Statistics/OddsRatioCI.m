c1d80d934469fcee303ac5bd1a1976e892b78966
BoettigerLab <boettiger.alistair@gmail.com>
2026-06-18 16:01:23 -0700

    updated OddsRatioCI to add option for Haldane-Anscombe correction and update CI handling and output contingency tables

--- matlab-functions/Statistics/OddsRatioCI.m
+++ matlab-functions/Statistics/OddsRatioCI.m
@@ -1,9 +1,16 @@
-function [odds,odds_CI,odds_stdev,oddsM] = OddsRatioCI(condition,exposure,varargin)
+function [odds,odds_CI,contengencyTable,odds_stdev,oddsM] = OddsRatioCI(condition,exposure,varargin)
+
+% change 05/21/2026 by Derek Le:
+% (1) output contengencyTable to see counts in the contingency table
+% (2) added Haldane-Anscombe correction to account for possibility of 0's 
+% in the 2x2 contingency tables
+
 
 % -------------------------------------------------------------------------
 % Default variables
 % -------------------------------------------------------------------------
 defaults = cell(0,3);
+defaults(end+1,:) = {'haldane', 'boolean', true};
 defaults(end+1,:) = {'cI', 'positive', .95};
 defaults(end+1,:) = {'iters', 'positive', 10000};
 % -------------------------------------------------------------------------
@@ -17,17 +24,23 @@ end
 % Parse variable input
 % -------------------------------------------------------------------------
 pars = ParseVariableArguments(varargin, defaults, mfilename);
-% pars = ParseVariableArguments([], defaults, mfilename);
-
-% condition = pab;
-% exposure = pad;
-
 
 
 a = sum(condition & exposure);
 d = sum(~condition & ~exposure);
 b = sum(~condition & exposure);
 c = sum(condition & ~exposure);
+
+contengencyTable = [a,b; c,d]; % aka confusion matrix 
+
+% Haldane-Anscombe correction: add 0.5 to every cell if any cell is zero
+if pars.haldane && (a==0 || b==0 || c==0 || d==0)
+    a = a + 0.5;
+    b = b + 0.5;
+    c = c + 0.5;
+    d = d + 0.5;
+end
+
 odds = a*d/(b*c);
 
 if nargout > 1 
@@ -42,6 +55,15 @@ if nargout > 1
     D = sum(~Cond & ~Exp, 2);
     B = sum(~Cond & Exp, 2);
     C = sum(Cond & ~Exp, 2);
+
+    % Haldane-Anscombe correction also for bootstrap replicates
+    if pars.haldane
+        zeroCell = (A==0) | (B==0) | (C==0) | (D==0);
+        A(zeroCell) = A(zeroCell) + 0.5;
+        B(zeroCell) = B(zeroCell) + 0.5;
+        C(zeroCell) = C(zeroCell) + 0.5;
+        D(zeroCell) = D(zeroCell) + 0.5;
+    end
 
     oddsM = A.*D./(B.*C);
     oddsM = sort(oddsM);
